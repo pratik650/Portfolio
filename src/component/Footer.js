3f1b779023ffb46d4f9032537145e4455262aeae
Pratik Kumbhkar <pratikprajapati650@gmail.com>
2022-12-27 19:23:19 +0530

    commit

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -8,40 +8,62 @@ export const Footer = () => {
   return (
     <div>
       <footer class="footer">
-  	 <div class="container">
-  	 	<div class="row">
-  	 		<div class="footer-col">
-  	 			<h4>Pratik Kumbhkar</h4>
-  	 			<ul>
-  	 				<li><a href="#">Hello I am an Enthusiast web developer and content creator I love to design the website with hight ui experience.</a></li>
-  	 				
-  	 			</ul>
-  	 		</div>
-  	 		<div class="footer-col">
-  	 			<h4>Contact us</h4>
-  	 			<ul>
-  	 				<li><a href="#">pratikprajapati650@gmail.com</a></li>
-  	 				<li><a href="#">+91-6266124546</a></li>
-  	 			</ul>
-  	 		</div>
-  	 		<div class="footer-col">
-  	 			<h4>Hire me!</h4>
-  	 			<ul>
-  	 				<li><a href="#">Resume</a></li>
-  	 			</ul>
-  	 		</div>
-  	 		<div class="footer-col">
-  	 			<h4>follow us</h4>
-  	 			<div class="social-links">
-  	 				<a href="#"><i class="fab fa-facebook-f"></i></a>
-  	 				<a href="#"><i class="fab fa-instagram"></i></a>
-  	 				<a href="#"><i class="fab fa-linkedin-in"></i></a>
-  	 			</div>
-  	 		</div>
-  	 	</div>
-  	 </div>
-  </footer>
-
+        <div class="container">
+          <div class="row">
+            <div class="footer-col">
+              <h4>Pratik Kumbhkar</h4>
+              <ul>
+                <li>
+                  Hello I am an Enthusiast web developer and content creator I
+                  love to design the website with hight ui experience.
+                </li>
+              </ul>
+            </div>
+            <div class="footer-col">
+              <h4>Contact us</h4>
+              <ul>
+                <li>
+                  <a class="mailto" href="mailto:pratikprajapati650@gmail.com">
+                    pratikprajapati650@gmail.com
+                  </a>
+                </li>
+                <li>
+                  <a href="tel:6266124546">+91-6266124546</a>
+                </li>
+              </ul>
+            </div>
+            <div class="footer-col">
+              <h4>Hire me!</h4>
+              <ul>
+                <li>
+                  <a href="https://drive.google.com/file/d/1rpL3f8p44zkWDhqvYietFCK0dErC1uQv/view?usp=sharing">
+                    Resume
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="footer-col">
+              <h4>follow us</h4>
+              <div class="social-links">
+                <a href="https://github.com/pratik650">
+                  <i class="fa-brands fa-github"></i>
+                </a>
+                <a href="https://www.instagram.com/pratik9485/">
+                  <i class="fab fa-instagram"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/pratik-kumbhkar-89a427214/">
+                  <i class="fab fa-linkedin-in"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+		<center><hr className='linelast'></hr></center>
+		
+        <div className='footer_bottom'>
+          <h5>© pratikprajapati designs</h5>
+        </div>
+      </footer>
     </div>
   );
 }
